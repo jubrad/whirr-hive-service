3ca8973a328f03422bf3b6633d59b9fd2e016ea2
jubrad <bradfield.justin7@gmail.com>
2014-02-16 18:31:30 -0500

    mvn clean install

--- target/classes/functions/install_hive.sh
+++ target/classes/functions/install_hive.sh
@@ -1,17 +1,30 @@
 set -x
 
-function install_hive(){
-    #curls and unpacks set version of hive to usr/local
-    curl -O http://www.dsgnwrld.com/am/hive/stable/hive-0.11.0.tar.gz
-    tar -C /usr/local/ -zxf hive-0.11.0.tar.gz
-    #sets the HIVE_HOME attribute for all users
-    echo "export HIVE_HOME=/usr/local/hive-0.11.0" >> /etc/profile
+function install_hive(){    
+    TARBALL_URL="$1"
+    # We'll use whirrs tarball install and pass it the correct url
+    install_tarball TARBALL_URL
+    
 
-    #adds hive to the path
-    cat > /etc/bin/hadoop <<EOF
+    #sets the HIVE_HOME attribute for all users
+    
+    HIVE_HOME=/usr/local/${basename TARBALL_URL .tar.gz}
+
+
+
+    echo "export HIVE_HOME=$HIVE_HOME" >> ~root/.bashrc
+    echo "export HIVE_HOME=$HADOOP_HOME" >> /etc/profile
+
+
+
+    #creates a symlink to the HIVE_HOME
+    ln -s $HIVE_HOME /usr/local/hive
+
+    #creates an exectuable in /bin to launch hive
+    cat > /bin/hive <<EOF
 #!/bin/sh
 
-exec /$HADOOP_HOME/bin/hive
+exec $HIVE_HOME/bin/hive $@
     
 EOF 
 }
